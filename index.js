d0f3eff599e2ca0e6076454d8fcaa38d31ce645a
Asteral <134489068+Asteral1@users.noreply.github.com>
2024-12-02 22:09:47 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'index.html')));
+
+// Route for the HTML page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Simple POST route
+app.post('/data', (req, res) => {
+    const data = req.body;
+    res.json({ message: 'Data received', data });
+});
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).send('Route not found');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
